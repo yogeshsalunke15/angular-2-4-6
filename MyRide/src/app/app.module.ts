b55ec74839098a6f9b0076d9d96947d3cd6b3241
Yogesh Salunke <yogeshsalunke15@users.noreply.github.com>
2018-08-14 08:27:44 +0530

    updated the local set up

--- MyRide/src/app/app.module.ts
+++ MyRide/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UserRegistrationComponent } from './user-registration/user-registration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserRegistrationComponent
   ],
   imports: [
     BrowserModule
