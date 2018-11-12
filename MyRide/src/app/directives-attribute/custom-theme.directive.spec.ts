0797aee54b945ac7ab1f1530a944ef34b6983b20
Yogesh Salunke <yogeshsalunke15@users.noreply.github.com>
2018-11-12 07:06:46 +0530

    removed unwanted folder

--- MyRide/src/app/directives-attribute/custom-theme.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { CustomThemeDirective } from './custom-theme.directive';
-
-describe('CustomThemeDirective', () => {
-  it('should create an instance', () => {
-    const directive = new CustomThemeDirective();
-    expect(directive).toBeTruthy();
-  });
-});
